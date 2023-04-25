0457367891f707bcaaaab859c097c8095a58a56d
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2023-04-25 12:19:40 -0500

    changes

--- L02/controllers/index.js
+++ L02/controllers/index.js
@@ -3,7 +3,7 @@ test1 = (req, res)=>{
     res.status(200).send(infor);
 };
 test2 = (req, res)=>{
-    const infor= "Gabriel Guerrero-2controller!";
+    const infor= "Gabriel Guerrero-1controller!";
     res.status(200).send(infor);
 };
 
