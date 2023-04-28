f7220e427ebf882d647743803650077397f97132
Gabe G <97242028+josegabrielguerrero26@users.noreply.github.com>
2023-04-27 19:20:06 -0500

    fixes

--- L02/controllers/contacts.js
+++ L02/controllers/contacts.js
@@ -1,8 +1,9 @@
+const { test1 } = require('.');
 const mongodb = require('../data_base/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
+  const result = await mongodb.getDb().db('test1').collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -13,7 +14,7 @@ const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
-    .db()
+    .db('test1')
     .collection('contacts')
     .find({ _id: userId });
   result.toArray().then((lists) => {
--- L02/routes/index.js
+++ L02/routes/index.js
@@ -1,12 +1,6 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
-const name= require('../controllers')
-
-router.get('/test1', name.test1);
-router.get('/test2', name.test2);
-router.get('/contacts', require('./contacts'))
-
-
-
+router.use('/contacts', require('./contacts'))
 
 module.exports = router;
